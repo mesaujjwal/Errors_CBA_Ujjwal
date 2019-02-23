aa8e2a60310ee01ba5e6a2869e6c2a2be224ef48
mesaujjwal <47854573+mesaujjwal@users.noreply.github.com>
2019-02-23 19:01:25 +0530

    Update errors.c

--- errors.c
+++ errors.c
@@ -10,38 +10,6 @@ int main()
   p1 = &m;
   int n = *p1; // Error on debug builds
   printf("%d", n); // access violation on release builds
-    
-    int* p1; // p1 can point to any location in memory
-  int m = 100;
-  p1 = &m; // error
-
-    
-    int* p1; // create a pointer to an integer
-  int m = 100;
-  p1 = &m; // assign address of m to p1
-  (*p1)++; // ERROR: we did not increment value of m
-  printf("%d\n", *p1);
-  printf("%d\n", m);
-
-    int* p1; // create a pointer to an integer
-  int m = 100;
-  p1 = &m;
-  
-   int* p1;
-  if ((p1 = (int*)malloc(sizeof(int))) == NULL)
-  {
-    return 1;
-  }
-  free(p1);
-  p1=NULL;
-  *p1 = 99;
-  free(p1);
-  //you stopped here
-    
-     const char arr[] = "hello";
-  const char *cp = arr;
-  printf("Size of arr %lu\n", (int)sizeof(arr));
-  printf("Size of *cp %lu\n", (int)sizeof(*cp));
 
   return 0;
 }
