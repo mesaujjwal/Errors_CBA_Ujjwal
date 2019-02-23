19b57923d5815ee7bf990246fd0c471f1ad884aa
mesaujjwal <47854573+mesaujjwal@users.noreply.github.com>
2019-02-23 18:44:33 +0530

    Update errors.c

--- errors.c
+++ errors.c
@@ -1,45 +1,42 @@
 int main()
 {
-  int* p1, p2;
+  int *p1, *p2;
   int n = 30;
   p1 = &n;
   p2 = &n; // error
     
    int* p1; // p1 can point to any location in memory
+  int m=10; 
+  p1 = &m;
   int n = *p1; // Error on debug builds
   printf("%d", n); // access violation on release builds
     
-     int* p1; // p1 can point to any location in memory
-  int m;
-  p1 = &m; // initialize pointer with an uninitialized variable
-  int n = *p1;
-  printf("%d", n); // huge negative number in debug and 0 in release on VC++
-    
     int* p1; // p1 can point to any location in memory
   int m = 100;
-  p1 = m; // error
+  p1 = &m; // error
 
     
     int* p1; // create a pointer to an integer
   int m = 100;
   p1 = &m; // assign address of m to p1
-  *p1++; // ERROR: we did not increment value of m
+  (*p1)++; // ERROR: we did not increment value of m
   printf("%d\n", *p1);
   printf("%d\n", m);
 
     int* p1; // create a pointer to an integer
   int m = 100;
   p1 = &m;
-  free(p1);//error - trying to free stack memory using free()
   
    int* p1;
   if ((p1 = (int*)malloc(sizeof(int))) == NULL)
   {
     return 1;
   }
+  free(p1);
+  p1=NULL;
   *p1 = 99;
   free(p1);
-  *p1 = 100; // BAD - undefined behavior
+  //you stopped here
   
    char* str1 = (char*)malloc(strlen("Thunderbird") + 1);
   strcpy_s(str1, strlen("Thunderbird") + 1, "Thunderbird");
